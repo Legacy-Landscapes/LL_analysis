ed6b19beb6baf6cb4d0de20fdae28d2b58a54ef9
AlkeVoskamp <alke.voskamp@senckenberg.de>
2021-04-15 15:14:52 +0200

    Removed old plotting code

--- Weighting the variables top sites map.R
+++ Weighting the variables top sites map.R
@@ -12,14 +12,8 @@ rm(list=ls())
 
 #-#-# Load libraries #-#-#
 library(ggplot2)
-
-
-#-#-# Load the world map #-#-#
-load_worldmap <- function(filename) {
-  worldmap <- sf::st_read(filename, layer = "ne_50m_admin_0_countries")
-  worldmap <- simplify_polygons(worldmap)
-  return(worldmap)
-}
+library(matrixStats)
+library(scales)
 
   
 #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# Weightig the different objectives #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
@@ -51,12 +45,12 @@ Wild_WClimPro <- 0
 Wild_weights <- as.data.frame(rbind(Wild_WBio,Wild_WClimStab,Wild_WLUStab,Wild_WWild,Wild_WArea,Wild_WClimPro))
 
 #---# Combination only #---#
-Comb_WBio <- 0
-Comb_WWild <- 1
-Comb_WClimStab <- 0
-Comb_WLUStab <- 0
-Comb_WArea <- 0
-Comb_WClimPro <- 0
+Comb_WBio <- 0.25
+Comb_WWild <- 0.25
+Comb_WClimStab <- 0.1
+Comb_WLUStab <- 0.1
+Comb_WArea <- 0.25
+Comb_WClimPro <- 0.05
 
 Comb_weights <- as.data.frame(rbind(Comb_WBio,Comb_WClimStab,Comb_WLUStab,Comb_WWild,Comb_WArea,Comb_WClimPro))
 
@@ -114,55 +108,329 @@ Ranks <- lapply(AllDF, function(d){
 })
 
 Map_table <- do.call(rbind,Ranks)
-head(Map_table)
+Plot_map <- Map_table[c("Int_Name", "Scenario", "Realm")]
+head(Plot_map)
 
 ## Save the ranking 
-write.csv(WeightTable,"Ranking_indomalaya_4Bio_2Wild_05ClimSt_05LUStab_2Size_1ClimP.csv")
-#write.csv(WeightTable20,"Ranking_palearctic_top20_100_ClimProt.csv")
+#write.csv(WeightTable,"Ranking_indomalaya_4Bio_2Wild_05ClimSt_05LUStab_2Size_1ClimP.csv")
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-# Plot the hotspots map #-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+#-#-# Set the file paths #-#-#
+world <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/Large_data_files/ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp"
+centroids <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/LL_analysis/Processed_data/Global_IUCN_WHS_KBA_centroids.csv"
+  
+  
+#-#-# Load the world map #-#-#
+worldmap <- load_worldmap(world)
+#africa <- subset(worldmap, CONTINENT == "Africa")
+#plot(africa)
+
+#-#-# Get the coordinates for the selected sites #-#-#
+sites <- read.csv(centroids)[2:4]
+colnames(sites) <- c("Int_Name", "x", "y")
+#selected <- Plot_map$Int_Name
+#selected_sites <- filter(sites, PAname %in% selected)
+selected_sites <- merge(Plot_map, sites, by = "Int_Name", all.x = T)
+head(selected_sites)
 
 
+#-#-# Nearctic #-#-# 
+selected_sites_n <- subset(selected_sites, Realm == "Nearctic")
+
+nearctic <- ggplot(selected_sites_n) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  coord_sf(xlim = c(-168, -17), ylim = c(12, 90), expand = FALSE) +
+  ## Add shortened equator line
+  geom_segment(
+    aes(x = -180, xend = -17, y = 0, yend = 0),
+    colour = "darkred",
+    linetype = "dashed") +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Nearctic") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(nearctic)
 
 
+#-#-# Neotropical #-#-# 
+neotropic <- ggplot(selected_sites) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  coord_sf(xlim = c(-90, -30), ylim = c(-55, 15), expand = FALSE) +
+  ## Add shortened equator line
+  geom_segment(
+    aes(x = -90, xend = -30, y = 0, yend = 0),
+    colour = "darkred",
+    linetype = "dashed") +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Neotropic") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(neotropic)
 
 
+#-#-# Palearctic #-#-# 
+palearctic <- ggplot(selected_sites) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  coord_sf(xlim = c(-12, 180), ylim = c(25, 83), expand = FALSE) +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Palearctic") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(palearctic)
 
 
-plot <- ggplot(selected_sites) +
-    geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
-    # Four types of points to show all and selected sites
-    geom_point(data = pa_centroids,
-               aes(x = x, y = y),
-               shape = 16,
-               size = 0.2,
-               colour = "grey90") +
-    geom_point(data = selected_sites,
-               aes(x = x, y = y, color = suitability),
-               shape = 18,
-               size = 2.5) +
-    scale_color_manual(name = "Suitability top sites:",
-                       values = c("Very high" = "red",
-                                  "High" = "orange",
-                                  "Good" = "gold")) +
-    coord_sf(xlim = c(-170, 180), ylim = c(-60, 90), expand = FALSE) +
-    # Add shortened equator line
-    geom_segment(
-      aes(x = -180, xend = 180, y = 0, yend = 0),
-      colour = "black",
-      linetype = "dashed"
-    ) +
-    theme(legend.key = element_blank(), legend.position = "bottom",
-          legend.title = element_text(size = 14, face = "bold"),
-          legend.text = element_text(size = 12)) +
-    guides(colour = guide_legend(override.aes = list(size = 5))) +
-    theme(axis.title = element_text(size = 16)) +
-    theme(panel.background = element_rect(fill = "white", colour = "white")) +
-    labs(x = "", y = "", title = "") +
-    theme(
-      axis.ticks.y = element_blank(),
-      axis.text.y = element_blank(),
-      axis.ticks.x = element_blank(),
-      axis.text.x = element_blank()) +
-    ggtitle(paste("Location top", n_sites, "sites", selection)) +
-    theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
-  return(plot)
+#-#-# Afrotropical #-#-# 
+afrotropic <- ggplot(selected_sites) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  coord_sf(xlim = c(-20, 60), ylim = c(-40, 20), expand = FALSE) +
+  ## Add shortened equator line
+  geom_segment(
+    aes(x = -20, xend = 60, y = 0, yend = 0),
+    colour = "darkred",
+    linetype = "dashed") +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Afrotropic") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(afrotropic)
+
+
+#-#-# Indomalayan #-#-# 
+indomalaya<- ggplot(selected_sites) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  coord_sf(xlim = c(67, 130), ylim = c(25, -9), expand = FALSE) +
+  ## Add shortened equator line
+  geom_segment(
+    aes(x = 67, xend = 130, y = 0, yend = 0),
+    colour = "darkred",
+    linetype = "dashed") +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Indomalaya") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(indomalaya)
+
+
+#-#-# Australasia #-#-# 
+#austrasia <- subset(worldmap, CONTINENT == "Oceania" | CONTINENT == "Asia")
+
+australasia <- ggplot(selected_sites) +
+  geom_sf(data = worldmap, fill = "black", color = "grey60", size = 0.2) +
+  coord_sf(xlim = c(110, 160), ylim = c(1, -50), expand = FALSE) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+  theme(legend.position = "none")+
+  guides(colour = guide_legend(override.aes = list(size = 5))) +
+  theme(axis.title = element_text(size = 16)) +
+  theme(panel.background = element_rect(fill = "white", colour = "white")) +
+  labs(x = "", y = "", title = "") +
+  theme(
+    axis.ticks.y = element_blank(),
+    axis.text.y = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()) +
+  ggtitle("Australasia") +
+  theme(plot.title = element_text(size = 21, face = "bold", hjust = 0))
+plot(australasia)
+
+
+## Create legend for the PCA plot
+## Function to extract plot legends
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
 }
+
+## ggplot to extract legend from for final plot
+legend_map <- ggplot(selected_sites) +
+  coord_sf(xlim = c(110, 160), ylim = c(1, -50), expand = FALSE) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 2.5) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "forestgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "grey")) +
+      theme(legend.key = element_blank(), legend.position = "bottom",
+            legend.title = element_text(size = 14, face = "bold"),
+            legend.text = element_text(size = 12)) +
+      guides(colour = guide_legend(override.aes = list(size = 5)))
+
+plot(legend_map)
+ScatterLegend <- g_legend(legend_map)
+plot(ScatterLegend)
+
+
+#-#-# Combine the PCA plots and the legend #-#-#  
+CombGlobal <- arrangeGrob(nearctic,palearctic,neotropic,afrotropic,indomalaya,australasia,ScatterLegend,ScatterLegend,
+                          widths = c(0.5,0.5),
+                          heights = c(1,1,1,0.2),
+                          ncol = 2,
+                          nrow = 4)
+plot(CombGlobal)
+
+#-#-# Save the final map #-#-#
+setwd("/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/Result_plots/")
+ggsave("Map Legacy Landscapes Conservation Objectives_separated realm plots.tiff",CombGlobal,width=25, height=15, unit="in", dpi=300, bg="white")
+
+
+#---#---#---# Other plot otion #---#---#---#
+#-#-# Set the file paths #-#-#
+realmpath <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Site selection analysis/Other files/Realms and Regions/CMEC regions & realms/WWF_Realms_gridded_05.Rdata"
+centroids <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/LL_analysis/Processed_data/Global_IUCN_WHS_KBA_centroids.csv"
+world <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/Large_data_files/ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp"
+centroids <- "/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/LL_analysis/Processed_data/Global_IUCN_WHS_KBA_centroids.csv"
+
+
+#-#-# Get the coordinates for the selected sites #-#-#
+sites <- read.csv(centroids)[2:4]
+colnames(sites) <- c("Int_Name", "x", "y")
+#selected <- Plot_map$Int_Name
+#selected_sites <- filter(sites, PAname %in% selected)
+selected_sites <- merge(Plot_map, sites, by = "Int_Name", all.x = T)
+head(selected_sites)
+
+
+#-#-# Load the world map #-#-#
+worldmap <- load_worldmap(world)
+
+
+#-#-# Load the realm map #-#-#
+realmmap <- get(load(realmpath))
+head(realmmap)
+realmmap <- na.omit(realmmap)
+realmmap <- subset(realmmap, !(RealmWWF == 2))
+realmmap <- subset(realmmap, !(RealmWWF == 7))
+
+realmmap$RealmName <- 0
+realmmap$RealmName[realmmap$RealmWWF == 1] <- "Australasia"
+realmmap$RealmName[realmmap$RealmWWF == 3] <- "Afrotropic"
+realmmap$RealmName[realmmap$RealmWWF == 4] <- "Indomalaya"
+realmmap$RealmName[realmmap$RealmWWF == 5] <- "Nearctic"
+realmmap$RealmName[realmmap$RealmWWF == 6] <- "Neotropic"
+realmmap$RealmName[realmmap$RealmWWF == 8] <- "Palearctic"
+
+colours <- c("steelblue3","orange3","navajowhite3","darkolivegreen3","plum4","brown")
+ 
+map <- ggplot(selected_sites) +
+  geom_raster(data = realmmap, aes( x = x, y = y, fill = as.factor(RealmName), alpha = 0.2)) +
+  scale_fill_manual("Realm",values = colours) +
+  geom_sf(data = worldmap, fill = NA, color = "grey60", size = 0.2) +
+  coord_sf(xlim = c(-170, 180), ylim = c(90, -60), expand = FALSE) +
+  geom_point(data = selected_sites,
+             aes(x = x, y = y, color = Scenario),
+             shape = 18,
+             size = 8,
+             alpha = 0.7) +
+  scale_color_manual(name = "Objective:",
+                     values = c("Biodiversity" = "darkgreen",
+                                "Wilderness" = "red",
+                                "Combination" = "black")) +
+  geom_segment(
+    aes(x = -180, xend = 180, y = 0, yend = 0), colour = "black", linetype = "dashed") +
+  theme(legend.key = element_blank(), legend.position = "bottom",
+        legend.title = element_text(size = 22, face = "bold"),
+        legend.text = element_text(size = 20)) +
+  theme(axis.text=element_text(size=20))+
+  theme(panel.background=element_rect(fill='white',colour="white"))+ # Remove the background
+  labs(x="", y="", title="") + # Remove axis titles
+  guides(colour = guide_legend(override.aes = list(size = 8)))
+
+plot(map) 
+
+#-#-# Save the final map #-#-#
+setwd("/Users/alkevoskamp/AG BGFM Dropbox/Voskamp/Legacy Landscapes/Legacy_landscapes_analysis/Result_plots/")
+ggsave("Map Legacy Landscapes Conservation Objectives.tiff",map,width=25, height=15, unit="in", dpi=300, bg="white")
+
+
+
